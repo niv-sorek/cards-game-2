a15168ab4062dcda559a0f89c6bd0663b26bebdf
Niv <sorekniv@gmail.com>
2020-11-22 21:56:06 +0200

    HW1 (Game and winner activities) - done.

--- app/src/main/java/com/example/cardswar/MainActivity.java
+++ app/src/main/java/com/example/cardswar/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import static android.content.pm.ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
 import static com.example.cardswar.Game.P1;
 import static com.example.cardswar.Game.P2;
 
@@ -19,37 +20,28 @@ public class MainActivity extends AppCompatActivity {
     private Button main_BTN_nextCard;
     private TextView txtPlayer1Score;
     private TextView txtPlayer2Score;
+    private ImageView main_IMG_avatarB;
+    private ImageView main_IMG_avatarA;
 
     Game gameManager;
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViews();
-        initViews();
-
-
-    }
-
-    private void initViews() {
+    private void startNewGame() {
         gameManager = new Game();
+        this.main_IMG_card2.setImageResource(R.drawable.img_card_blank);
+        this.main_IMG_card1.setImageResource(R.drawable.img_card_blank);
+        main_BTN_nextCard.setEnabled(true);
+        main_BTN_nextCard.setText(R.string.play);
         updateScore();
-        main_BTN_nextCard.setOnClickListener(v -> nextTurn());
     }
-
 
     public void nextTurn() {
         if (!gameManager.isFinished()) {
-
             Card[] cards = gameManager.playTurn();
-
             this.main_IMG_card1.setImageResource(this.getResources().getIdentifier("drawable/" + cards[P1].getCardFileName(), null, this.getPackageName()));
             this.main_IMG_card2.setImageResource(this.getResources().getIdentifier("drawable/" + cards[P2].getCardFileName(), null, this.getPackageName()));
 
-
             updateScore();
-        }
-       else {
+        } else {
             main_BTN_nextCard.setEnabled(false);
             main_BTN_nextCard.setText(R.string.game_finished);
             showWinner();
@@ -67,13 +59,33 @@ public class MainActivity extends AppCompatActivity {
         this.txtPlayer2Score.setText("" + gameManager.getScores()[P2]);
     }
 
-    private void findViews() {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViews();
+        initViews();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startNewGame();
+    }
+
+    private void initViews() {
+        this.setRequestedOrientation(SCREEN_ORIENTATION_LANDSCAPE);
+        this.main_BTN_nextCard.setOnClickListener(v -> nextTurn());
+        this.main_IMG_avatarA.setImageResource(R.drawable.img_avatar_1);
+        this.main_IMG_avatarB.setImageResource(R.drawable.img_avatar_2);
+    }
+
+    private void findViews() {
         this.main_IMG_card1 = findViewById(R.id.main_IMG_card1);
         this.main_IMG_card2 = findViewById(R.id.main_IMG_card2);
         this.main_BTN_nextCard = findViewById(R.id.main_BTN_nextCard);
         this.txtPlayer1Score = findViewById((R.id.main_LBL_p1_score));
         this.txtPlayer2Score = findViewById((R.id.main_LBL_p2_score));
+        this.main_IMG_avatarB = findViewById(R.id.main_IMG_avatarB);
+        this.main_IMG_avatarA = findViewById(R.id.main_IMG_avatarA);
     }
-
 }
--- app/src/main/java/com/example/cardswar/WinnerActivity.java
+++ app/src/main/java/com/example/cardswar/WinnerActivity.java
@@ -1,10 +1,11 @@
 package com.example.cardswar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import static com.example.cardswar.Game.P1;
 import static com.example.cardswar.Game.P2;
@@ -12,6 +13,7 @@ import static com.example.cardswar.Game.P2;
 public class WinnerActivity extends AppCompatActivity {
     private TextView winner_LBL_name;
     private int winner;
+    private Button winner_BTN_newGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +27,19 @@ public class WinnerActivity extends AppCompatActivity {
 
 
     private void initViews() {
-        if(this.winner == P1)
+        if (this.winner == P1)
             this.winner_LBL_name.setText(R.string.player_a_name);
         else if (this.winner == P2)
             this.winner_LBL_name.setText(R.string.player_b_name);
         else this.winner_LBL_name.setText(R.string.tie_message);
+
+        this.winner_BTN_newGame.setOnClickListener(v -> {finish();
+
+        });
     }
 
     private void findViews() {
         winner_LBL_name = findViewById(R.id.winner_lbl_winnerName);
+        this.winner_BTN_newGame = findViewById(R.id.winner_BTN_newGame);
     }
 }
